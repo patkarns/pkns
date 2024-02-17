a14a5c57777a1c5cea4fe9a4e3b6ee79c72dca5a
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-16 21:20:42 -0800

    change [landing-page]: add resume and email links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import resume from './Resume_Pat_K.pdf';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hi! :)
+      <a href={resume} download="Resume_Pat_Karns.pdf">Resume</a>
+      < a href="mailto:karns.napat@gmail.com">karns.napat@gmail.com</a>
+
     </div>
   );
 }
