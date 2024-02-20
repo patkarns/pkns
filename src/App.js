e521e4519a89bd5960c3dc1683947ef0754c61bd
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-20 15:34:14 -0800

    Change [landing-page]: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-// import MenuIcon from '@mui/icons-material/Menu';
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home.tsx";
 import "./App.scss";
-import BakingExperiments from "./pages/baking-experiments/baking-experiments.tsx";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FlouristBrownies from './pages/baking-experiments/recipes/flourist-brownies.tsx';
 
 
 function App() {
@@ -12,7 +8,6 @@ function App() {
     <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="baking-experiments" element={<BakingExperiments />}/>
         </Routes>
 
     </div>
